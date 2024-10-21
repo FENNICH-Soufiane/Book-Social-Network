89cd41eba2c5e4fa48921f5ad434b93968b5b7ad
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-10-21 11:38:16 +0100

    Implement the User details Service

---
+++ src/main/java/com/project/book_network/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.project.book_network.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.project.book_network.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return repository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found") );
+    }
+    
+}
