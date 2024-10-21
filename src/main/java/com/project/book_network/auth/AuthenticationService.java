6bc757e27da7ec98fe69b6bd0c2eb693b7029767
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-10-21 22:54:35 +0100

    Create the authentication controller

---
+++ src/main/java/com/project/book_network/auth/AuthenticationService.java
@@ -0,0 +1,19 @@
+package com.project.book_network.auth;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.validation.Valid;
+
+@Service
+public class AuthenticationService {
+
+    public void register(@Valid RegistrationRequest request) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'register'");
+    }
+
+    // public void register(RegistrationRequest request) {
+
+    // }
+
+}
