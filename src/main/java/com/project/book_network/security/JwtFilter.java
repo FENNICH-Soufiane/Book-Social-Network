041b42593284baef30446321645f7e3736140ac7
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-10-21 13:03:16 +0100

    Implement the JwtFilter class

--- src/main/java/com/project/book_network/security/JwtFilter.java
+++ src/main/java/com/project/book_network/security/JwtFilter.java
@@ -1,8 +1,43 @@
 package com.project.book_network.security;
 
+import java.io.IOException;
+import java.net.http.HttpHeaders;
+
 import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.micrometer.common.lang.NonNull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 @Service
-public class JwtFilter {
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService ;
+
+    @Override
+    protected void doFilterInternal(
+                                    @NonNull HttpServletRequest request, 
+                                    @NonNull HttpServletResponse response, 
+                                    @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        if(request.getServletPath().contains("/api/v1/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+    }
 
 }
