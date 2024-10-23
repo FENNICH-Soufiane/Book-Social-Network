25ebc4d904b690bffbe32383b1fcd31a90d092d1
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-10-23 16:25:51 +0100

    Implement the register method

--- src/main/java/com/project/book_network/auth/AuthenticationService.java
+++ src/main/java/com/project/book_network/auth/AuthenticationService.java
@@ -1,19 +1,75 @@
 package com.project.book_network.auth;
 
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.cglib.core.Local;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import jakarta.validation.Valid;
+import com.project.book_network.role.RoleRepository;
+import com.project.book_network.user.Token;
+import com.project.book_network.user.TokenRepository;
+import com.project.book_network.user.User;
+import com.project.book_network.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
 
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
 
-    public void register(@Valid RegistrationRequest request) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'register'");
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+
+    public void register(RegistrationRequest request) {
+        var userRole = roleRepository.findByName("USER")
+        //todo - better exception handling
+            .orElseThrow(() -> new IllegalStateException("Role USER was not initialized"));
+        
+            var user = User.builder()
+                        .firstname(request.getFirstname())
+                        .lastName(request.getLastname())
+                        .email(request.getEmail())
+                        .password(passwordEncoder.encode(request.getPassword()))
+                        .accountLocked(false)
+                        .enabled(false)
+                        .roles(List.of(userRole))
+                        .build();
+            userRepository.save(user);
+            sendValidationEmail(user);
     }
 
-    // public void register(RegistrationRequest request) {
+    private void sendValidationEmail(User user) {
+        var newToken = generateAndSaveActivationToken(user);
+        // send email
+    }
 
-    // }
+    private String generateAndSaveActivationToken(User user) {
+        String generateToken = generateActivationCode(6);
+        var token = Token.builder()
+                    .token(generateToken)
+                    .createdAt(LocalDateTime.now())
+                    .createdAt(LocalDateTime.now().plusMinutes(15))
+                    .user(user)
+                    .build();
+        tokenRepository.save(token);
+        return generateToken;
+    }
+
+    private String generateActivationCode(int length) {
+        String characters = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+        SecureRandom secureRandom = new SecureRandom();
+        for(int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(characters.length());
+            codeBuilder.append(characters.charAt((randomIndex)));
+        }
+        return codeBuilder.toString();
+    }
 
 }
